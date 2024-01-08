5e139c786e393f9fe2efe5ca370e689a5d53eb57
eboe62 <eboe62@hotmail.com>
2024-01-07 19:47:04 -0500

    Capítulo 7 Ejercicio 1

--- labs/reliability-ha/load-test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-while true
-do
-  curl long-load-reliability-ha.apps.ocp4.example.com/health
-  sleep 2s
-done
--- labs/reliability-probes/load-test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-while true
-do
-  curl long-load-reliability-probes.apps.ocp4.example.com/health
-  sleep 2s
-done
